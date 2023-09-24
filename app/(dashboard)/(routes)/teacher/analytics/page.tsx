1ab2b66c5686c5f6bd002f3461a23cbb86d77a79
Kamrul Saad <skamrul2680@gmail.com>
2023-09-25 03:52:23 +0600

    Added Analytics Dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AnalyticsPage = () => {
+import { getAnalytics } from "@/actions/getAnalytics";
+
+import { DataCard } from "./_components/dataCard";
+import { Chart } from "./_components/chart";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
   return (
-    <div>AnalyticsPage</div>
-  )
-}
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
+};
 
-export default AnalyticsPage
+export default AnalyticsPage;
